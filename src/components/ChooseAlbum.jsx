22af7653bf15b7bcf7da3d173122e76e2eaa693f
irina <rakhimovairinav@gmail.com>
2024-08-24 18:38:30 +0300

    Bug fixed

--- src/components/ChooseAlbum.jsx
+++ src/components/ChooseAlbum.jsx
@@ -7,28 +7,45 @@ const ChooseAlbum = ({ token }) => {
     
     function getRandomSongFromPlaylist(playlistId) {
         const url = `https://api.spotify.com/v1/playlists/${playlistId}`;
-      
+    
         return fetch(url, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
-        .then(response => {
-            if (!response.ok) {
-                if (response.status === 403) {
-                console.log(token);    
-                throw new Error("Access forbidden. Check if the playlist is available or the token has the right scopes.");
-                }
-                throw new Error(`Error: ${response.status} - ${response.statusText}`);
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            // Continue processing data
-        })
-        .catch(error => {
-            console.error("Error fetching random track:", error.message);
+            const totalTracks = data.tracks.total;
+            if (totalTracks > 0) {
+                const randomOffset = Math.floor(Math.random() * totalTracks);
+    
+                const trackUrl = `${url}/tracks?offset=${randomOffset}&limit=1`;
+    
+                return fetch(trackUrl, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.items && data.items.length > 0) {
+                        const track = data.items[0].track;
+                        return {
+                            id: track.id,
+                            name: track.name,
+                            artist: track.artists[0].name,
+                            album: track.album.name,
+                            image: track.album.images[0].url,
+                            uri: track.uri,
+                        };
+                    }
+                    console.log(token);
+                    return null;
+                });
+            }
+            return null;
         });
     }
   
