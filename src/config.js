ff7cf8c9831a59c9b3070c8acf3bfd65e71c1f4d
irina <rakhimovairinav@gmail.com>
2024-08-25 21:27:10 +0300

    Redirect corrected

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "f10f5c3ad1f04b79993338e581e3c01a";
-export const redirectUri = "http://localhost:3000/callback";
+export const redirectUri = "https://songs-guesser.onrender.com";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
